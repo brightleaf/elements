7540b02707eb0919b9b808ccd8027dcc25672d8f
Kevin Isom <kevin.isom@gmail.com>
2019-06-24 21:01:39 +1200

    Notification

---
+++ src/notification/__tests__/notification.test.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import {
+  render,
+  cleanup,
+  fireEvent,
+  waitForElement,
+} from '@testing-library/react'
+import { toBeInTheDocument, toHaveClass } from 'jest-dom'
+import { Notification } from '../notification'
+
+expect.extend({ toBeInTheDocument, toHaveClass })
+describe('Column and Columns', () => {
+  afterEach(cleanup)
+  it('should render', () => {
+    const { container } = render(
+      <Notification>
+        <div>Hi</div>
+      </Notification>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+  })
+  it('should render isInfo with class is-info', () => {
+    const { container } = render(
+      <Notification isInfo>
+        <div>Hi</div>
+      </Notification>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toHaveClass('is-info')
+  })
+  /*
+     isMobile,
+  className,
+  isFluid,
+  isWideScreen,
+  isFullHD,
+  */
+  it('should render isSuccess with class is-success', () => {
+    const { container } = render(
+      <Notification isSuccess>
+        <div>Hi</div>
+      </Notification>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toHaveClass('is-success')
+  })
+  it('should render isDanger with class is-danger', () => {
+    const { container } = render(
+      <Notification isDanger>
+        <div>Hi</div>
+      </Notification>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toHaveClass('is-danger')
+  })
+  it('should render isPrimary with class is-primary', () => {
+    const { container } = render(
+      <Notification isPrimary>
+        <div>Hi</div>
+      </Notification>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toHaveClass('is-primary')
+  })
+  it('should render isLink with class is-link', () => {
+    const { container } = render(
+      <Notification isLink>
+        <div>Hi</div>
+      </Notification>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toHaveClass('is-link')
+  })
+  it('should render isWarning with class is-warning', () => {
+    const { container } = render(
+      <Notification isWarning>
+        <div>Hi</div>
+      </Notification>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toHaveClass('is-warning')
+  })
+  it('should not render with when isShow is false', async () => {
+    const mockDismiss = jest.fn(() => {})
+    const { container, getByTitle } = render(
+      <Notification isShown isPrimary onDismiss={mockDismiss}>
+        <div>Hi</div>
+      </Notification>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+
+    fireEvent.click(getByTitle(/dismiss notification/i))
+    const notificationNode = await waitForElement(() =>
+      getByTitle('notification element')
+    )
+    expect(notificationNode).toMatchSnapshot()
+    expect(mockDismiss).toHaveBeenCalled()
+  })
+})
---
+++ src/notification/notification.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { CSSTransition } from 'react-transition-group'
+import classnames from 'classnames'
+import { Colors } from '../modifiers'
+
+export const Notification = ({
+  isShown,
+  children,
+  onDismiss,
+  isPrimary,
+  isSuccess,
+  isInfo,
+  isWarning,
+  isDanger,
+  isLink,
+}) => {
+  const classes = Colors({
+    isPrimary,
+    isSuccess,
+    isInfo,
+    isWarning,
+    isDanger,
+    isLink,
+  })
+  return (
+    <CSSTransition
+      in={isShown}
+      timeout={300}
+      classNames="alert"
+      unmountOnExit
+      onEnter={() => console.log('onEnter')}
+      onExited={() => console.log('onExited')}
+    >
+      <div
+        className={classnames('notification', classes)}
+        title="notification element"
+      >
+        <button
+          title="dismiss notification"
+          className="notification-dismiss"
+          onClick={e => {
+            e.preventDefault()
+            onDismiss()
+          }}
+        ></button>
+        {children}
+      </div>
+    </CSSTransition>
+  )
+}
+
+export default Notification
+
+Notification.defaultProps = {
+  isShown: true,
+  isPrimary: false,
+  isSuccess: false,
+  isInfo: false,
+  isWarning: false,
+  isDanger: false,
+  isLink: false,
+  onClose: () => {},
+  onExit: () => {},
+  onDismiss: () => {},
+}
