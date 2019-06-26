f10f6a8afda5a7391a4871e2f4425d483103e0c8
Kevin Isom <kevin.isom@gmail.com>
2019-06-27 08:07:50 +1200

    export more card elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 export { Columns, Column, FullColumn } from './columns'
 export { Container } from './container'
 export { Notification } from './notification'
-export { Card, CardBody, CardFooter, CardHeader, CardImage } from './card'
+export {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardImage,
+  CardFooterItem,
+} from './card'
+export { AutoComplete } from './autocomplete'
