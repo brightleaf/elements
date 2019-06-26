c0a7128d92f01aafc509a0b9114b4fb75cc148e2
Kevin Isom <kevin.isom@gmail.com>
2019-06-27 08:03:46 +1200

    handle css imports for tests

--- jest.config.js
+++ jest.config.js
@@ -74,7 +74,11 @@ module.exports = {
   //   "tsx",
   //   "node"
   // ],
-
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
 
