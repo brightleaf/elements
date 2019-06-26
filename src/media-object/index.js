79360d938c2687037ac049e4241679b4ef7c43c0
Kevin Isom <kevin.isom@gmail.com>
2019-06-27 08:07:27 +1200

    Start on media object

---
+++ src/media-object/index.js
@@ -0,0 +1,1 @@
+export { MediaObject, MediaLeft } from './media-object'
---
+++ src/media-object/media-object.js
@@ -0,0 +1,51 @@
+import React from 'react'
+export const MediaLeft = ({ children }) => {
+  return (
+    <figure className="media-left">
+      <p className="image is-64x64">
+        <img src="https://bulma.io/images/placeholders/128x128.png" />
+      </p>
+    </figure>
+  )
+}
+export const MediaObject = ({ src }) => {
+  return (
+    <article className="media">
+      <MediaLeft />
+      <div className="media-content">
+        <div className="content">
+          <p>
+            <strong>John Smith</strong> <small>@johnsmith</small>{' '}
+            <small>31m</small>
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+            non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+        <nav className="level is-mobile">
+          <div className="level-left">
+            <a className="level-item">
+              <span className="icon is-small">
+                <i className="fas fa-reply"></i>
+              </span>
+            </a>
+            <a className="level-item">
+              <span className="icon is-small">
+                <i className="fas fa-retweet"></i>
+              </span>
+            </a>
+            <a className="level-item">
+              <span className="icon is-small">
+                <i className="fas fa-heart"></i>
+              </span>
+            </a>
+          </div>
+        </nav>
+      </div>
+      <div className="media-right">
+        <button className="delete"></button>
+      </div>
+    </article>
+  )
+}
