8394148e0fc78c540d9c3ebb26474d173c8216e0
Kevin Isom <kevin.isom@gmail.com>
2019-06-26 17:57:38 +1200

    Working notification

--- src/notification/notification.js
+++ src/notification/notification.js
@@ -1,83 +1,108 @@
-import React, { useCallback, useState } from 'react'
-import { CSSTransition } from 'react-transition-group'
+import React, {
+  useRef,
+  useEffect,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+} from 'react'
 import classnames from 'classnames'
 import { Colors } from '../modifiers'
 import './notification.css'
+import '../css/animations.css'
 
-export const Notification = ({
-  isShown,
-  children,
-  onDismiss,
-  isPrimary,
-  isSuccess,
-  isInfo,
-  isWarning,
-  isDanger,
-  isLink,
-}) => {
-  const classes = Colors({
-    isPrimary,
-    isSuccess,
-    isInfo,
-    isWarning,
-    isDanger,
-    isLink,
-  })
-  // console.info('isShow?', isShown)
-  const [show, setShow] = useState(false)
-  const trig = useCallback(() => {
-    console.log('trigged', !show)
-    return setShow(!show)
-  }, [])
-  // console.info('show state?', show)
-  if (isShown && !show) {
-    console.info('that thing', isShown)
-    console.log('that show state', show)
-    trig()
+export const Notification = forwardRef(
+  (
+    {
+      isShown,
+      children,
+      onDismiss,
+      onDismissed,
+      isPrimary,
+      isSuccess,
+      isInfo,
+      isWarning,
+      isDanger,
+      isLink,
+    },
+    ref
+  ) => {
+    const classes = Colors({
+      isPrimary,
+      isSuccess,
+      isInfo,
+      isWarning,
+      isDanger,
+      isLink,
+    })
+
+    const [show, setShow] = useState(!!isShown)
+
+    const animation = {
+      animation: true,
+      fadeIn: show,
+      fadeOutLeft: !show,
+      'is-hidden': !isShown,
+    }
+
+    const container = useRef()
+
+    useEffect(() => {
+      const onEnd = ({ target, animationName }) => {
+        if (animationName.indexOf('fadeOut') > -1) {
+          target.classList.add('is-hidden')
+          onDismissed()
+        }
+      }
+      const onStart = ({ target, animationName }) => {
+        target.classList.remove('is-hidden')
+      }
+      if (container.current) {
+        container.current.addEventListener('animationend', onEnd)
+        container.current.addEventListener('animationstart', onStart)
+        return () => {
+          container.current.removeEventListener('animationend', onEnd)
+          container.current.removeEventListener('animationstart', onStart)
+        }
+      }
+    }, [show])
+
+    useImperativeHandle(ref, () => ({
+      show: function() {
+        setShow(true)
+      },
+      hide: function() {
+        setShow(false)
+      },
+    }))
+
+    return (
+      <div
+        ref={container}
+        onAnimationEnd={({ target, animationName }) => {
+          console.info('End Animation', { target, animationName })
+        }}
+        onAnimationStart={({ target, animationName }) => {
+          console.info('Start Animation', { target, animationName })
+        }}
+        className={classnames('notification', 'animated', animation, classes)}
+        title="notification element"
+      >
+        <button
+          title="dismiss notification"
+          className="delete notification-dismiss"
+          onClick={e => {
+            e.preventDefault()
+            console.info('dismiss click', show)
+            setShow(false)
+            console.info('dismiss click after', show)
+            onDismiss()
+          }}
+        ></button>
+        {children}
+      </div>
+    )
   }
-  return (
-    <>
-      {' '}
-      {show && (
-        <CSSTransition
-          in={show}
-          timeout={300}
-          classNames="notification"
-          onEnter={element => {
-            console.info('onEnter', element)
-            element.classList.remove('is-hidden')
-            // element.classList.remove('notification-exit-done')
-          }}
-          onEntered={() => console.info('onEntered')}
-          onExit={() => console.info('onExit')}
-          onExited={element => {
-            console.info('onExited', element)
-            element.classList.add('is-hidden')
-            // element.classList.remove('notification-exit-done')
-          }}
-        >
-          <div
-            className={classnames('notification', classes)}
-            title="notification element"
-          >
-            <button
-              title="dismiss notification"
-              className="delete notification-dismiss"
-              onClick={e => {
-                e.preventDefault()
-                console.info('dismiss click', show)
-                setShow(false)
-                console.info('dismiss click after', show)
-                trig()
-              }}
-            ></button>
-            {children}
-          </div>
-        </CSSTransition>
-      )}
-    </>
-  )
-}
+)
 
 export default Notification
 
@@ -92,4 +117,5 @@ Notification.defaultProps = {
   onClose: () => {},
   onExit: () => {},
   onDismiss: () => {},
+  onDismissed: () => {},
 }
