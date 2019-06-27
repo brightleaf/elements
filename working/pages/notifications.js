933ebf2a338c89245aab4d3dfa33f385369e4562
Kevin Isom <kevin.isom@gmail.com>
2019-06-27 14:04:28 +1200

    Update to use components and dismiss

--- working/pages/notifications.js
+++ working/pages/notifications.js
@@ -1,57 +1,55 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+/* eslint-disable spellcheck/spell-checker */
 import React, { useRef, useState } from 'react'
-
-import {
-  Container,
-  Columns,
-  Column,
-  FullColumn,
-  Notification,
-} from '@brightleaf/elements'
+import { Container, Columns, Column, FullColumn, Notification } from '../../src'
 export default () => {
   const [showNotification, setShowNotification] = useState(false)
   const notifRef = useRef()
 
   return (
     <Container className="App content">
-      <FullColumn>
-        <Notification
-          ref={notifRef}
-          isPrimary
-          isShown={showNotification}
-          onDismissed={e => {
-            console.log('onDismiss hit')
-            setShowNotification(!showNotification)
-          }}
-        >
-          Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem
-          ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis
-          placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
-          fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a>{' '}
-          efficitur. Sit amet, consectetur adipiscing elit
-        </Notification>
-        {!showNotification && (
-          <button
-            className="button"
-            onClick={e => {
+      <Columns>
+        <Column isOneThird>
+          <Notification
+            ref={notifRef}
+            isPrimary
+            isShown={showNotification}
+            onDismissed={e => {
+              console.log('onDismiss hit')
               setShowNotification(!showNotification)
-              notifRef.current.show()
             }}
           >
-            Show
-          </button>
-        )}
-        {showNotification && (
-          <button
-            className="button"
-            onClick={e => {
-              setShowNotification(!showNotification)
-              notifRef.current.hide()
-            }}
-          >
-            Hide
-          </button>
-        )}
-      </FullColumn>
+            Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem
+            ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis
+            placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+            fringilla. Nullam gravida purus diam, et dictum{' '}
+            <a>felis venenatis</a> efficitur. Sit amet, consectetur adipiscing
+            elit
+          </Notification>
+          {!showNotification && (
+            <button
+              className="button"
+              onClick={e => {
+                setShowNotification(!showNotification)
+                notifRef.current.show()
+              }}
+            >
+              Show
+            </button>
+          )}
+          {showNotification && (
+            <button
+              className="button"
+              onClick={e => {
+                setShowNotification(!showNotification)
+                notifRef.current.hide()
+              }}
+            >
+              Hide
+            </button>
+          )}
+        </Column>
+      </Columns>
       <FullColumn>
         <Notification isPrimary isShown>
           Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem
@@ -100,6 +98,24 @@ export default () => {
       <FullColumn>
         <Notification isLink isShown>
           Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem
+          ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis
+          placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+          fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a>{' '}
+          efficitur. Sit amet, consectetur adipiscing elit
+        </Notification>
+      </FullColumn>
+      <FullColumn>
+        <Notification isLight isShown>
+          Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem
+          ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis
+          placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+          fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a>{' '}
+          efficitur. Sit amet, consectetur adipiscing elit
+        </Notification>
+      </FullColumn>
+      <FullColumn>
+        <Notification isLight isShown isDismissible={false}>
+          <p className="title">{`isDismissible={false}`}</p>
           ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis
           placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
           fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a>{' '}
