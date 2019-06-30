76751d65d8a6abb5b725ef81d8b3240a899b876b
Kevin Isom <kevin.isom@gmail.com>
2019-07-01 11:33:13 +1200

    Level components

---
+++ src/level/index.js
@@ -0,0 +1,5 @@
+import { Level, LevelItem, LevelLeft, LevelRight } from './level'
+
+export { Level, LevelItem, LevelLeft, LevelRight }
+
+export default Level
--- src/level/level.js
+++ src/level/level.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import classnames from 'classnames'
 
 export const LevelLeft = ({ children }) => {
   return <div className="level-left">{children}</div>
@@ -6,47 +7,17 @@ export const LevelLeft = ({ children }) => {
 export const LevelRight = ({ children }) => {
   return <div className="level-right">{children}</div>
 }
-export const LevelItem = ({ children }) => {
-  return <div className="level-item">{children}</div>
+export const LevelItem = ({ className, children, hasTextCentered }) => {
+  const kid = React.cloneElement(children, {
+    className: classnames('level-item', className, {
+      'has-text-centered': hasTextCentered,
+    }),
+  })
+  return <>{kid}</>
 }
-export const Level = () => {
-  return (
-    <nav className="level">
-      <div className="level-left">
-        <div className="level-item">
-          <p className="subtitle is-5">
-            <strong>123</strong> posts
-          </p>
-        </div>
-        <div className="level-item">
-          <div className="field has-addons">
-            <p className="control">
-              <input className="input" type="text" placeholder="Find a post" />
-            </p>
-            <p className="control">
-              <button className="button">Search</button>
-            </p>
-          </div>
-        </div>
-      </div>
 
-      <div className="level-right">
-        <p className="level-item">
-          <strong>All</strong>
-        </p>
-        <p className="level-item">
-          <a>Published</a>
-        </p>
-        <p className="level-item">
-          <a>Drafts</a>
-        </p>
-        <p className="level-item">
-          <a>Deleted</a>
-        </p>
-        <p className="level-item">
-          <a className="button is-success">New</a>
-        </p>
-      </div>
-    </nav>
-  )
+export const Level = ({ children, isMobile }) => {
+  const classNames = classnames('level', { 'is-mobile': isMobile })
+
+  return <nav className={classNames}>{children}</nav>
 }
