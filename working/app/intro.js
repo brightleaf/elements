ba3682e85d9def692f7db5a776ff136b05fdadd2
Kevin Isom <kevin.isom@gmail.com>
2019-07-01 18:25:20 +1200

    elements not hooks

--- working/app/intro.js
+++ working/app/intro.js
@@ -4,8 +4,8 @@ export default function Intro() {
   return (
     <div>
       <br />
-      <h1>Brightleaf React Hooks</h1>
-      <div>Useful React Hooks</div>
+      <h1>Brightleaf Elements</h1>
+      <div>Bulma styled UI Elements</div>
       <hr />
     </div>
   )
