2fd03c925b315ea2e1669325c0151764fb908090
Kevin Isom <kevin.isom@gmail.com>
2019-07-01 15:51:46 +1200

    section

---
+++ src/section/index.js
@@ -0,0 +1,5 @@
+import { Section } from './section'
+
+export { Section }
+
+export default Section
---
+++ src/section/section.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Section = ({ children }) => {
+  return <section className="section">{children}</section>
+}
+
+export default Section
