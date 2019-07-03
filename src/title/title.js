69e65dda69f7a6a7a074e99a2780c458a5e3e56d
Kevin Isom <kevin.isom@gmail.com>
2019-07-03 12:53:08 +1200

    Title component

---
+++ src/title/title.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react'
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+import { HeroContext } from '../hero'
+export const Title = ({
+  as: Hn,
+  className,
+  is,
+  isSpaced,
+  children,
+  ...props
+}) => {
+  const value = useContext(HeroContext)
+  const isInHero = value && value.isInHero
+
+  const classes = {
+    [`is-${is}`]: !isInHero && !!is,
+    'is-spaced': isSpaced,
+  }
+
+  return (
+    <Hn className={classnames('title', className, classes)} {...props}>
+      {children}
+    </Hn>
+  )
+}
+
+Title.propTypes = {
+  as: PropTypes.node,
+  className: PropTypes.string,
+  isSpaced: PropTypes.bool,
+  is: PropTypes.oneOf(['1', '2', '3', '4', '5', '6', false]),
+}
+
+Title.defaultProps = {
+  as: 'h1',
+  is: false,
+  isSpaced: false,
+}
