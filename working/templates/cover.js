8365742a453e456700124123a16d1097a7550571
Kevin Isom <kevin.isom@gmail.com>
2019-07-04 23:57:26 +1200

    Cover Template

---
+++ working/templates/cover.js
@@ -0,0 +1,122 @@
+import React from 'react'
+import {
+  BaseIcon,
+  Box,
+  Button,
+  Card,
+  CardImageContainer,
+  CardBody,
+  Column,
+  Columns,
+  Container,
+  Content,
+  Footer,
+  Hero,
+  HeroHead,
+  HeroBody,
+  HeroFooter,
+  Image,
+  NavBar,
+  NavBarEnd,
+  NavBarMenu,
+  NavBarBrand,
+  NavBarItem,
+  Icon,
+  Tile,
+  Title,
+  SubTitle,
+  Tag,
+  Tabs,
+  TabItem,
+} from '../../src/'
+import { useStyleSheet, useStyles } from './hook'
+
+export const CoverTemplate = () => {
+  useStyleSheet(
+    'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+  )
+  useStyleSheet('https://fonts.googleapis.com/css?family=Open+Sans')
+  useStyles(`
+  html,
+  body {
+      font-family: 'Open Sans';
+  }
+
+  img {
+      padding: 5px;
+      border: 1px solid #ccc;
+  }
+  `)
+  return (
+    <Hero isFullHeight isDefault isBold>
+      <HeroHead>
+        <NavBar>
+          <Container>
+            <NavBarBrand
+              src="https://bulmatemplates.github.io/bulma-templates/images/bulma.png"
+              href="#"
+            ></NavBarBrand>
+            <NavBarMenu id="navbarMenu">
+              <NavBarEnd>
+                <Tabs isRight>
+                  <TabItem isActive>
+                    <a>Home</a>
+                  </TabItem>
+                  <TabItem>
+                    <a href="">Examples</a>
+                  </TabItem>
+                  <TabItem>
+                    <a href="">Features</a>
+                  </TabItem>
+                  <TabItem>
+                    <a href="">Team</a>
+                  </TabItem>
+                  <TabItem>
+                    <a href="">Help</a>
+                  </TabItem>
+                </Tabs>
+              </NavBarEnd>
+            </NavBarMenu>
+          </Container>
+        </NavBar>
+      </HeroHead>
+      <HeroBody>
+        <Container className="has-text-centered">
+          <Columns className="is-vcentered">
+            <Column size="5">
+              <Image
+                is4by3
+                src="https://picsum.photos/800/600/?random"
+                alt="Description"
+              />
+            </Column>
+            <Column size="6" className="is-offset-1">
+              <Title is="2">Superhero Scaffolding</Title>
+              <SubTitle is="4">
+                Let this cover page describe a product or service.
+              </SubTitle>
+              <br />
+              <p className="has-text-centered">
+                <Button isAnchor isMedium isOutlined isInfo>
+                  Learn more
+                </Button>
+              </p>
+            </Column>
+          </Columns>
+        </Container>
+      </HeroBody>
+
+      <HeroFooter>
+        <Container>
+          <Tabs isCentered>
+            <TabItem>
+              <a>And this is the bottom</a>
+            </TabItem>
+          </Tabs>
+        </Container>
+      </HeroFooter>
+    </Hero>
+  )
+}
+
+export default CoverTemplate
