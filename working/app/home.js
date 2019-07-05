cd1a60473ee3ce1e761a29e10f8e5835e03a7206
Kevin Isom <kevin.isom@gmail.com>
2019-07-06 10:30:13 +1200

    Show initial example

--- working/app/home.js
+++ working/app/home.js
@@ -1,5 +1,59 @@
 import React from 'react'
-
+import { useStyles, useStyleSheet } from '@brightleaf/react-hooks'
+import Highlight from 'react-highlight'
+import {
+  Box,
+  Column,
+  Columns,
+  Content,
+  Hero,
+  HeroBody,
+  Title,
+  SubTitle,
+} from '../../src/'
 export default function Home() {
-  return <div className="content" />
+  useStyleSheet('https://fonts.googleapis.com/css?family=Open+Sans')
+  useStyles(`
+  html,
+  body {
+      font-family: 'Open Sans';
+  }
+  `)
+  useStyleSheet('code.css')
+  return (
+    <Content>
+      <Columns>
+        <Column isOneThird>
+          <Hero isWarning>
+            <HeroBody>
+              <Title>Brightleaf Elements</Title>
+              <SubTitle as="h4">Bulma Styled React Components</SubTitle>
+            </HeroBody>
+          </Hero>
+        </Column>
+        <Column isTwoThirds>
+          <Highlight className="bash">
+            {'npm install @brightleaf/elements'}
+          </Highlight>
+          <Highlight className="javascript">
+            {`
+import React from 'react'
+import { Hero, HeroBody } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Hero isWarning>
+      <HeroBody>
+        <Title>Brightleaf Elements</Title>
+        <SubTitle as="h4">Bulma Styled React Components</SubTitle>
+      </HeroBody>
+    </Hero>
+  )
+}
+`}
+          </Highlight>
+        </Column>
+      </Columns>
+    </Content>
+  )
 }
