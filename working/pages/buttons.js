7f7c33eea172c7a30387139fef7bc0ad189cbde9
Kevin Isom <kevin.isom@gmail.com>
2019-07-06 10:29:45 +1200

    Add example and info

--- working/pages/buttons.js
+++ working/pages/buttons.js
@@ -1,56 +1,277 @@
 /* eslint-disable spellcheck/spell-checker */
 /* eslint-disable sonarjs/no-duplicate-string */
 import React from 'react'
-import { Box, Button, Buttons, FullColumn } from '../../src'
-
+import { useStyleSheet, useStyles } from '@brightleaf/react-hooks'
+import Highlight from 'react-highlight'
+import {
+  Box,
+  Button,
+  Buttons,
+  Columns,
+  Column,
+  Container,
+  FullColumn,
+  Content,
+} from '../../src'
+import { Snippet } from '../components/snippet'
 const Callout = ({ children }) => {
   return <div className="callout is-primary">{children}</div>
 }
 export default () => {
+  useStyleSheet('https://fonts.googleapis.com/css?family=Open+Sans')
+  useStyles(`
+  html,
+  body {
+      font-family: 'Open Sans';
+  }
+  `)
+  useStyleSheet('code.css')
   return (
     <>
-      <Box>
-        <Buttons>
-          <Button isPrimary>Primary Button</Button>
-          <Button isInfo>Info Button</Button>
-          <Button isDanger>Danger Button</Button>
-          <Button isLink>Link Button</Button>
-          <Button isWarning>Warning Button</Button>
-          <Button isSuccess>Success Button</Button>
-        </Buttons>
-      </Box>
-      <Box>
-        <Buttons>
-          <Button isPrimary isSubmit>
-            Primary Submit Button
-          </Button>
-          <Button isInfo isAnchor href="#">
-            Info Anchor Button
-          </Button>
-          <Button isDanger isReset>
-            Danger Reset Button
-          </Button>
-          <Button isLink isAnchor href="#">
-            Anchor Link Button
-          </Button>
-        </Buttons>
-      </Box>
-      <Box>
-        <Buttons>
-          <Button isSmall>Small</Button>
-          <Button>Default</Button>
-          <Button isNormal>Normal</Button>
-          <Button isMedium>Medium</Button>
-          <Button isLarge>Large</Button>
-        </Buttons>
-      </Box>
-      <Box>
-        <Buttons areMedium>
-          <Button>All</Button>
-          <Button>Medium</Button>
-          <Button>Size</Button>
-        </Buttons>
-      </Box>
+      <FullColumn>
+        <Content>
+          <p>
+            The <strong>button</strong> is an essential element of any design.
+            It&apos;s meant to look and behave as an{' '}
+            <strong>interactive</strong> element of your page.
+          </p>
+        </Content>
+      </FullColumn>
+      <Snippet>
+        <Columns>
+          <Column isHalf className="snippet-preview">
+            <Container>
+              <Button>Button</Button>
+            </Container>
+          </Column>
+          <Column isHalf>
+            <Highlight className="javascript">
+              {`
+import React from 'react'
+import { Button } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Container>
+      <Button>Button</Button>
+    </Container>
+  )
+}
+`}
+            </Highlight>
+          </Column>
+        </Columns>
+      </Snippet>
+      <FullColumn>
+        <Content>
+          <h3>Colors</h3>
+        </Content>
+      </FullColumn>
+      <Snippet>
+        <Columns>
+          <Column isHalf className="snippet-preview">
+            <Container>
+              <Buttons>
+                <Button isPrimary>Primary</Button>
+                <Button isInfo>Info</Button>
+                <Button isDanger>Danger</Button>
+                <Button isLink>Link</Button>
+                <Button isWarning>Warning</Button>
+                <Button isSuccess>Success</Button>
+              </Buttons>
+            </Container>
+          </Column>
+          <Column isHalf>
+            <Highlight className="javascript">
+              {`
+import React from 'react'
+import { Buttons, Button } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Buttons>
+      <Button isPrimary>Primary</Button>
+      <Button isInfo>Info</Button>
+      <Button isDanger>Danger</Button>
+      <Button isLink>Link</Button>
+      <Button isWarning>Warning</Button>
+      <Button isSuccess>Success</Button>
+    </Buttons>
+  )
+}
+`}
+            </Highlight>
+          </Column>
+        </Columns>
+      </Snippet>
+      <FullColumn>
+        <Content>
+          <p>
+            The <code>Button</code> component can be used to render:
+          </p>
+          <ul>
+            <li>
+              <code>&lt;a&gt;</code> anchor links with <code>isAnchor</code>
+            </li>
+            <li>
+              <code>&lt;button&gt;</code> form buttons is the default
+            </li>
+            <li>
+              <code>&lt;input type="submit"&gt;</code> submit inputs with{' '}
+              <code>isSubmit</code>
+            </li>
+            <li>
+              <code>&lt;input type="reset"&gt;</code> reset inputs with{' '}
+              <code>isReset</code>
+            </li>
+          </ul>
+        </Content>
+      </FullColumn>
+      <Snippet>
+        <Columns>
+          <Column isHalf className="snippet-preview">
+            <Container>
+              <Buttons>
+                <Button>Button</Button>
+                <Button isSubmit>Submit</Button>
+                <Button isAnchor href="#">
+                  Anchor
+                </Button>
+                <Button isReset>Reset</Button>
+              </Buttons>
+            </Container>
+          </Column>
+          <Column isHalf>
+            <Highlight className="javascript">
+              {`
+import React from 'react'
+import { Buttons, Button } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Buttons>
+      <Button>Button</Button>
+      <Button isSubmit>Submit</Button>
+      <Button isAnchor href="#">
+        Anchor
+      </Button>
+      <Button isReset>Reset</Button>
+    </Buttons>
+  )
+}
+`}
+            </Highlight>
+          </Column>
+        </Columns>
+      </Snippet>
+      <FullColumn>
+        <Content>
+          <p>
+            The button comes in <strong>4 different sizes:</strong>
+          </p>
+          <ul>
+            <li>
+              small <code>isSmall</code>
+            </li>
+            <li>
+              normal <code>isNormal</code>
+            </li>
+            <li>
+              medium <code>isMedium</code>
+            </li>
+            <li>
+              large <code>isLarge</code>
+            </li>
+          </ul>
+          <p>
+            While the default size is the <strong>normal</strong> one, the{' '}
+            <code>isNormal</code> modifier exists in case you need to reset the
+            button to its normal size.
+          </p>
+        </Content>
+      </FullColumn>
+      <Snippet>
+        <Columns>
+          <Column isHalf className="snippet-preview">
+            <Buttons>
+              <Button isSmall>Small</Button>
+              <Button>Default</Button>
+              <Button isNormal>Normal</Button>
+              <Button isMedium>Medium</Button>
+              <Button isLarge>Large</Button>
+            </Buttons>
+          </Column>
+          <Column isHalf>
+            <Highlight className="javascript">
+              {`
+import React from 'react'
+import { Buttons, Button } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Buttons>
+      <Button isSmall>Small</Button>
+      <Button>Default</Button>
+      <Button isNormal>Normal</Button>
+      <Button isMedium>Medium</Button>
+      <Button isLarge>Large</Button>
+    </Buttons>
+  )
+}
+`}
+            </Highlight>
+          </Column>
+        </Columns>
+      </Snippet>
+      <FullColumn>
+        <Content>
+          <p>
+            You can change the size of <strong>multiple buttons</strong> at once
+            by wrapping them in a <code>Buttons</code> parent component, and
+            applying one of 3 modifiers:
+          </p>
+          <ul>
+            <li>
+              <code>areSmall</code>
+            </li>
+            <li>
+              <code>areMedium</code>
+            </li>
+            <li>
+              <code>areLarge</code>
+            </li>
+          </ul>
+        </Content>
+      </FullColumn>
+      <Snippet>
+        <Columns>
+          <Column isHalf className="snippet-preview">
+            <Buttons areMedium>
+              <Button>All</Button>
+              <Button>Medium</Button>
+              <Button>Size</Button>
+            </Buttons>
+          </Column>
+          <Column isHalf>
+            <Highlight className="javascript">
+              {`
+import React from 'react'
+import { Buttons, Button } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Buttons areMedium>
+      <Button>All</Button>
+      <Button>Medium</Button>
+      <Button>Size</Button>
+    </Buttons>
+    )
+}
+`}
+            </Highlight>
+          </Column>
+        </Columns>
+      </Snippet>
       <Box>
         <Buttons areSmall>
           <Button>Small</Button>
