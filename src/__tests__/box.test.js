f4abbc528fcd3d19062533a3b44c408785b4cc96
Kevin Isom <kevin.isom@gmail.com>
2019-07-09 09:00:50 +1200

    Box tests

---
+++ src/__tests__/box.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import { toHaveClass } from 'jest-dom'
+import { Box } from '../'
+
+expect.extend({ toHaveClass })
+
+describe('Box component', () => {
+  afterEach(cleanup)
+  it('should render', () => {
+    const { container } = render(
+      <Box>
+        <div>Trapped in a Box</div>
+      </Box>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toHaveClass('box')
+  })
+  it('should render with extra classnames', () => {
+    const { container } = render(
+      <Box className="boxed-in">
+        <div>Trapped in a Box</div>
+      </Box>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toHaveClass('box')
+    expect(container.firstChild).toHaveClass('boxed-in')
+  })
+})
