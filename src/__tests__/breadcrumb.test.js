bb3aa86b313caef56ef783a3106f4cb50976bf87
Kevin Isom <kevin.isom@gmail.com>
2019-07-09 09:00:59 +1200

    breadcrumb tests

---
+++ src/__tests__/breadcrumb.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import { toHaveClass } from 'jest-dom'
+import { Breadcrumb, BreadcrumbItem } from '../'
+
+expect.extend({ toHaveClass })
+
+describe('Breadcrumb component', () => {
+  afterEach(cleanup)
+  it('should render', () => {
+    const { container } = render(
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <a>Home</a>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <a>Components</a>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <a>Hero</a>
+        </BreadcrumbItem>
+      </Breadcrumb>
+    )
+    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toHaveClass('breadcrumb')
+  })
+})
