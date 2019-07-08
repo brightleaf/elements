f382d2b5ce58cf69de634bfc7d4a957c276ddd73
Kevin Isom <kevin.isom@gmail.com>
2019-07-09 09:00:43 +1200

    Autocomplete tests

---
+++ src/__tests__/autcomplete.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import { toHaveClass } from 'jest-dom'
+import { AutoComplete } from '../'
+
+expect.extend({ toHaveClass })
+
+describe('AutoComplete component', () => {
+  afterEach(cleanup)
+  it('should render', () => {
+    const items = [
+      { name: 'First', id: 1 },
+      { name: 'Second', id: 2 },
+      { name: 'Third', id: 3 },
+    ]
+    const { container } = render(
+      <AutoComplete
+        list={items}
+        onSelect={item => {}}
+        onValueChange={e => {}}
+      />
+    )
+    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toHaveClass('autocomplete')
+  })
+})
