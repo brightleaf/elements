ceb0fb798c38bdfa1915aa1ade37a7af9f346d84
Kevin Isom <kevin.isom@gmail.com>
2019-07-09 13:13:44 +1200

    Use Prism for highlighting

--- working/components/highlighter.js
+++ working/components/highlighter.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
-import BareHighlight from 'react-fast-highlight/lib/BareHighlight'
-import hljs from '../app/highlight'
+import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/prism-light'
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx'
+import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism'
+
+SyntaxHighlighter.registerLanguage('jsx', jsx)
 
 export default class Highlight extends Component {
   render() {
     const { children, ...props } = this.props
     return (
-      <BareHighlight {...props} highlightjs={hljs}>
+      <SyntaxHighlighter
+        language="jsx"
+        style={prism}
+        showLineNumbers={false}
+        wrapLines={true}
+      >
         {children}
-      </BareHighlight>
+      </SyntaxHighlighter>
     )
   }
 }
