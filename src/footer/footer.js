a8869f7ae1ea57fa05bca3574c5146921866af7e
Kevin Isom <kevin.isom@gmail.com>
2019-07-23 13:08:11 +1200

    support the classes

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import classnames from 'classnames'
-const Footer = ({ children, className }) => {
-  return <footer className={classnames('footer', className)}>{children}</footer>
+import { allTheClasses } from '../modifiers'
+
+const Footer = ({ children, className, ...props }) => {
+  const classes = allTheClasses(props)
+  return (
+    <footer className={classnames('footer', className, classes)}>
+      {children}
+    </footer>
+  )
 }
 
 export { Footer }
