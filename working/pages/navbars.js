bac55161ee7579e84bd6a8fc0087588f9b015796
Kevin Isom <kevin.isom@gmail.com>
2019-07-25 12:13:28 +1200

    Examples and info and collapse panel

--- working/pages/navbars.js
+++ working/pages/navbars.js
@@ -14,7 +14,15 @@ import {
   NavBarStart,
   NavBarMenu,
   Section,
+  Hero,
+  HeroBody,
+  Title,
+  SubTitle,
 } from '../../src'
+
+import { Collapse } from '../components/collapse'
+import Highlight from '../components/highlighter'
+import { ComponentsTabs } from '../components/tabs'
 
 const Example = ({ children, isPrimary }) => {
   return <div className="example">{children}</div>
@@ -22,6 +30,16 @@ const Example = ({ children, isPrimary }) => {
 export default () => {
   return (
     <Section>
+      <Hero isLight isBold>
+        <HeroBody>
+          <Title>Modal</Title>
+          <SubTitle as="p" is="4">
+            A classic <strong>modal</strong> overlay, in which you can include
+            any content you want
+          </SubTitle>
+        </HeroBody>
+      </Hero>
+      <ComponentsTabs />
       <Example>
         <NavBar>
           <NavBarBrand
@@ -76,6 +94,64 @@ export default () => {
             </NavBarEnd>
           </NavBarMenu>
         </NavBar>
+        <Collapse>
+          <Highlight
+            className="javascript"
+            languages={['javascript']}
+          >{`import React from 'react'
+import { NavBar,NavBarBrand,NavBarDivider,NavBarDropDown,NavBarEnd,NavBarItem,NavBarStart,NavBarMenu, } from '@brightleaf/elements'
+export default () => {
+  return (
+    <NavBar>
+      <NavBarBrand
+        src="brightleafjs-logo.png"
+        href="https://brightleaf.dev"
+        target="navbarBasicExample"
+        width="112"
+        height="28"
+      />
+      <NavBarMenu id="navbarBasicExample">
+        <NavBarStart>
+          <NavBarItem><a>Home</a></NavBarItem>
+          <NavBarItem><a>Documentation</a></NavBarItem>
+          <NavBarDropDown title="Examples">
+            <NavBarItem><a to="/containers">Container</a></NavBarItem>
+            <NavBarItem>
+              <a to="/columns">Columns</a>
+            </NavBarItem>
+            <NavBarItem>
+              <a to="/notifications">Notifications</a>
+            </NavBarItem>
+            <NavBarItem>
+              <a to="/autocomplete">AutoComplete</a>
+            </NavBarItem>
+            <NavBarDivider />
+            <NavBarItem>
+              <a>Report an issue</a>
+            </NavBarItem>
+          </NavBarDropDown>
+        </NavBarStart>
+
+        <NavBarEnd>
+          <NavBarItem>
+            <div>
+              <Buttons>
+                <Button isPrimary isAnchor>
+                  <strong>Sign up</strong>
+                </Button>
+                <Button isLight isAnchor>
+                  Log in
+                </Button>
+              </Buttons>
+            </div>
+          </NavBarItem>
+        </NavBarEnd>
+      </NavBarMenu>
+    </NavBar>
+  )
+}
+        `}</Highlight>
+        </Collapse>
       </Example>
       <Example>
         <NavBar isPrimary>
@@ -131,7 +207,65 @@ export default () => {
             </NavBarEnd>
           </NavBarMenu>
         </NavBar>
+        <Collapse>
+          <Highlight
+            className="javascript"
+            languages={['javascript']}
+          >{`import React from 'react'
+import { NavBar,NavBarBrand,NavBarDivider,NavBarDropDown,NavBarEnd,NavBarItem,NavBarStart,NavBarMenu, } from '@brightleaf/elements'
+export default () => {
+  return (
+    <NavBar isPrimary>
+      <NavBarBrand
+        src="brightleafjs-logo.png"
+        href="https://brightleaf.dev"
+        target="navbarBasicExample"
+        width="112"
+        height="28"
+      />
+      <NavBarMenu id="navbarBasicExample">
+        <NavBarStart>
+          <NavBarItem><a>Home</a></NavBarItem>
+          <NavBarItem><a>Documentation</a></NavBarItem>
+          <NavBarDropDown title="Examples">
+            <NavBarItem><a to="/containers">Container</a></NavBarItem>
+            <NavBarItem>
+              <a to="/columns">Columns</a>
+            </NavBarItem>
+            <NavBarItem>
+              <a to="/notifications">Notifications</a>
+            </NavBarItem>
+            <NavBarItem>
+              <a to="/autocomplete">AutoComplete</a>
+            </NavBarItem>
+            <NavBarDivider />
+            <NavBarItem>
+              <a>Report an issue</a>
+            </NavBarItem>
+          </NavBarDropDown>
+        </NavBarStart>
+
+        <NavBarEnd>
+          <NavBarItem>
+            <div>
+              <Buttons>
+                <Button isPrimary isAnchor>
+                  <strong>Sign up</strong>
+                </Button>
+                <Button isLight isAnchor>
+                  Log in
+                </Button>
+              </Buttons>
+            </div>
+          </NavBarItem>
+        </NavBarEnd>
+      </NavBarMenu>
+    </NavBar>
+  )
+}`}</Highlight>
+        </Collapse>
       </Example>
+
       <Example>
         <NavBar isLight>
           <NavBarBrand
@@ -186,6 +320,63 @@ export default () => {
             </NavBarEnd>
           </NavBarMenu>
         </NavBar>
+        <Collapse>
+          <Highlight
+            className="javascript"
+            languages={['javascript']}
+          >{`import React from 'react'
+import { NavBar,NavBarBrand,NavBarDivider,NavBarDropDown,NavBarEnd,NavBarItem,NavBarStart,NavBarMenu, } from '@brightleaf/elements'
+export default () => {
+  return (
+    <NavBar isLight>
+      <NavBarBrand
+        src="brightleafjs-logo.png"
+        href="https://brightleaf.dev"
+        target="navbarBasicExample"
+        width="112"
+        height="28"
+      />
+      <NavBarMenu id="navbarBasicExample">
+        <NavBarStart>
+          <NavBarItem><a>Home</a></NavBarItem>
+          <NavBarItem><a>Documentation</a></NavBarItem>
+          <NavBarDropDown title="Examples">
+            <NavBarItem><a to="/containers">Container</a></NavBarItem>
+            <NavBarItem>
+              <a to="/columns">Columns</a>
+            </NavBarItem>
+            <NavBarItem>
+              <a to="/notifications">Notifications</a>
+            </NavBarItem>
+            <NavBarItem>
+              <a to="/autocomplete">AutoComplete</a>
+            </NavBarItem>
+            <NavBarDivider />
+            <NavBarItem>
+              <a>Report an issue</a>
+            </NavBarItem>
+          </NavBarDropDown>
+        </NavBarStart>
+
+        <NavBarEnd>
+          <NavBarItem>
+            <div>
+              <Buttons>
+                <Button isPrimary isAnchor>
+                  <strong>Sign up</strong>
+                </Button>
+                <Button isLight isAnchor>
+                  Log in
+                </Button>
+              </Buttons>
+            </div>
+          </NavBarItem>
+        </NavBarEnd>
+      </NavBarMenu>
+    </NavBar>
+  )
+}`}</Highlight>
+        </Collapse>
       </Example>
       <Example>
         <NavBar isInfo>
@@ -241,6 +432,63 @@ export default () => {
             </NavBarEnd>
           </NavBarMenu>
         </NavBar>
+        <Collapse>
+          <Highlight
+            className="javascript"
+            languages={['javascript']}
+          >{`import React from 'react'
+import { NavBar,NavBarBrand,NavBarDivider,NavBarDropDown,NavBarEnd,NavBarItem,NavBarStart,NavBarMenu, } from '@brightleaf/elements'
+export default () => {
+  return (
+    <NavBar isInfo>
+      <NavBarBrand
+        src="brightleafjs-logo.png"
+        href="https://brightleaf.dev"
+        target="navbarBasicExample"
+        width="112"
+        height="28"
+      />
+      <NavBarMenu id="navbarBasicExample">
+        <NavBarStart>
+          <NavBarItem><a>Home</a></NavBarItem>
+          <NavBarItem><a>Documentation</a></NavBarItem>
+          <NavBarDropDown title="Examples">
+            <NavBarItem><a to="/containers">Container</a></NavBarItem>
+            <NavBarItem>
+              <a to="/columns">Columns</a>
+            </NavBarItem>
+            <NavBarItem>
+              <a to="/notifications">Notifications</a>
+            </NavBarItem>
+            <NavBarItem>
+              <a to="/autocomplete">AutoComplete</a>
+            </NavBarItem>
+            <NavBarDivider />
+            <NavBarItem>
+              <a>Report an issue</a>
+            </NavBarItem>
+          </NavBarDropDown>
+        </NavBarStart>
+
+        <NavBarEnd>
+          <NavBarItem>
+            <div>
+              <Buttons>
+                <Button isPrimary isAnchor>
+                  <strong>Sign up</strong>
+                </Button>
+                <Button isLight isAnchor>
+                  Log in
+                </Button>
+              </Buttons>
+            </div>
+          </NavBarItem>
+        </NavBarEnd>
+      </NavBarMenu>
+    </NavBar>
+  )
+}`}</Highlight>
+        </Collapse>
       </Example>
       <Example>
         <NavBar isTransparent>
@@ -332,6 +580,63 @@ export default () => {
             </NavBarEnd>
           </NavBarMenu>
         </NavBar>
+        <Collapse>
+          <Highlight
+            className="javascript"
+            languages={['javascript']}
+          >{`import React from 'react'
+import { NavBar,NavBarBrand,NavBarDivider,NavBarDropDown,NavBarEnd,NavBarItem,NavBarStart,NavBarMenu, } from '@brightleaf/elements'
+export default () => {
+  return (
+    <NavBar isTransparent>
+      <NavBarBrand
+        src="brightleafjs-logo.png"
+        href="https://brightleaf.dev"
+        target="navbarBasicExample"
+        width="112"
+        height="28"
+      />
+      <NavBarMenu id="navbarBasicExample">
+        <NavBarStart>
+          <NavBarItem><a>Home</a></NavBarItem>
+          <NavBarItem><a>Documentation</a></NavBarItem>
+          <NavBarDropDown title="Examples">
+            <NavBarItem><a to="/containers">Container</a></NavBarItem>
+            <NavBarItem>
+              <a to="/columns">Columns</a>
+            </NavBarItem>
+            <NavBarItem>
+              <a to="/notifications">Notifications</a>
+            </NavBarItem>
+            <NavBarItem>
+              <a to="/autocomplete">AutoComplete</a>
+            </NavBarItem>
+            <NavBarDivider />
+            <NavBarItem>
+              <a>Report an issue</a>
+            </NavBarItem>
+          </NavBarDropDown>
+        </NavBarStart>
+
+        <NavBarEnd>
+          <NavBarItem>
+            <div>
+              <Buttons>
+                <Button isPrimary isAnchor>
+                  <strong>Sign up</strong>
+                </Button>
+                <Button isLight isAnchor>
+                  Log in
+                </Button>
+              </Buttons>
+            </div>
+          </NavBarItem>
+        </NavBarEnd>
+      </NavBarMenu>
+    </NavBar>
+  )
+}`}</Highlight>
+        </Collapse>
       </Example>
     </Section>
   )
