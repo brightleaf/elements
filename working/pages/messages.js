70acc3a9f86d03dd890e694a337143e6431e8daf
Kevin Isom <kevin.isom@gmail.com>
2019-07-25 12:12:54 +1200

    Show code examples and give info

--- working/pages/messages.js
+++ working/pages/messages.js
@@ -9,137 +9,336 @@ import {
   Message,
   MessageBody,
   MessageHeader,
+  Section,
+  Hero,
+  HeroBody,
+  Title,
+  SubTitle,
 } from '../../src'
+import { ComponentsTabs } from '../components/tabs'
+import { Snippet } from '../components/snippet'
+import Highlight from '../components/highlighter'
 
 export default () => {
   return (
-    <Content>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message>
-              <MessageHeader>Messages</MessageHeader>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message isPrimary>
-              <MessageHeader>Messages</MessageHeader>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message isSuccess>
-              <MessageHeader>Messages</MessageHeader>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message isDanger>
-              <MessageHeader>Messages</MessageHeader>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message isWarning>
-              <MessageHeader>Messages</MessageHeader>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message>
-              <MessageHeader hasDelete={false}>Messages</MessageHeader>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message isLarge>
-              <MessageHeader hasDelete={false}>Messages</MessageHeader>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message isSmall>
-              <MessageHeader hasDelete={false}>Small Messages</MessageHeader>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message isMedium>
-              <MessageHeader hasDelete={false}>Medium Messages</MessageHeader>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message isPrimary>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message isDanger>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-      <Columns>
-        <Column isHalf>
-          <Box>
-            <Message isWarning>
-              <MessageBody>Message body</MessageBody>
-            </Message>
-          </Box>
-        </Column>
-      </Columns>
-    </Content>
+    <Section className="App">
+      <Hero isLight isBold>
+        <HeroBody>
+          <Title>Message</Title>
+          <SubTitle as="p" is="4">
+            Colored <strong>message</strong> blocks, to emphasize part of your
+            page
+          </SubTitle>
+        </HeroBody>
+      </Hero>
+      <ComponentsTabs />
+      <Content>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message>
+                <MessageHeader>Messages</MessageHeader>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message>
+      <MessageHeader>Messages</MessageHeader>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message isPrimary>
+                <MessageHeader>Messages</MessageHeader>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message isPrimary>
+      <MessageHeader>Messages</MessageHeader>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message isSuccess>
+                <MessageHeader>Messages</MessageHeader>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message isSuccess>
+      <MessageHeader>Messages</MessageHeader>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message isDanger>
+                <MessageHeader>Messages</MessageHeader>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message isDanger>
+      <MessageHeader>Messages</MessageHeader>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message isWarning>
+                <MessageHeader>Messages</MessageHeader>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message isWarning>
+      <MessageHeader>Messages</MessageHeader>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message>
+                <MessageHeader hasDelete={false}>Messages</MessageHeader>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message>
+      <MessageHeader hasDelete={false}>Messages</MessageHeader>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message isLarge>
+                <MessageHeader hasDelete={false}>Messages</MessageHeader>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message isLarge>
+      <MessageHeader hasDelete={false}>Messages</MessageHeader>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message isSmall>
+                <MessageHeader hasDelete={false}>Small Messages</MessageHeader>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message isSmall>
+      <MessageHeader hasDelete={false}>Small Messages</MessageHeader>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message isMedium>
+                <MessageHeader hasDelete={false}>Medium Messages</MessageHeader>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message isMedium>
+      <MessageHeader hasDelete={false}>Medium Messages</MessageHeader>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message isPrimary>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message isPrimary>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message isDanger>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message isDanger>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+        <Columns>
+          <Column isOneThird>
+            <Box>
+              <Message isWarning>
+                <MessageBody>Message body</MessageBody>
+              </Message>
+            </Box>
+          </Column>
+          <Column isTwoThirds>
+            <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import { Message, MessageHeader, MessageBody } from '@brightleaf/elements'
+export default () => {
+  return (
+    <Message isWarning>
+      <MessageBody>Message body</MessageBody>
+    </Message>
+  )
+}
+          `}</Highlight>
+          </Column>
+        </Columns>
+      </Content>
+    </Section>
   )
 }
