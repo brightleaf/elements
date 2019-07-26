85fdcea23d548b8332a072cf4dc75a25fde345a6
Kevin Isom <kevin.isom@gmail.com>
2019-07-26 22:35:42 +1200

    inbox example complete

--- working/templates/inbox.js
+++ working/templates/inbox.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classnames from 'classnames'
 import { useStyleSheet } from '@brightleaf/react-hooks/lib/use-stylesheet'
-
+import { useScript } from '@brightleaf/react-hooks/lib/use-script'
+import { useGet } from '@brightleaf/react-hooks/lib/use-get'
 import {
   BaseIcon,
   Box,
@@ -24,7 +26,10 @@ import {
   NavBarEnd,
   NavBarMenu,
   NavBarBrand,
+  NavBarBurger,
+  NavBarDropDown,
   NavBarItem,
+  NavBarDivider,
   Icon,
   Tile,
   Title,
@@ -34,227 +39,297 @@ import {
   TabItem,
 } from '../../src/'
 
-export const InboxTemplate = () => {
-  useStyleSheet(
-    'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+const MessageCard = ({ active, ...msg }) => {
+  return (
+    <Card onClick={msg.onClick} className={active ? 'active' : ''}>
+      <CardBody>
+        <div className="msg-header">
+          <span className="msg-from">
+            <small>From: {msg.from}</small>
+          </span>
+          <span className="msg-timestamp">
+            {new Date(msg.timestamp).toDateString()}
+          </span>
+          <span className="msg-attachment">
+            <i className="fa fa-paperclip"></i>
+          </span>
+        </div>
+        <div className="msg-subject">
+          <span className="msg-subject">
+            <strong>{msg.subject}</strong>
+          </span>
+        </div>
+        <div className="msg-snippet">
+          <p>{msg.snippet}</p>
+        </div>
+      </CardBody>
+    </Card>
   )
+}
+export const InboxTemplate = () => {
   useStyleSheet('https://fonts.googleapis.com/css?family=Open+Sans:300,400,700')
   useStyleSheet('inbox.css')
+  useScript('font-awesome.js')
+  const [message, setMessage] = useState({
+    from: '',
+    timestamp: null,
+    subject: '',
+    snippet: '',
+    fullMail: '',
+    email: '',
+    picked: true,
+  })
+
+  const { data, error, loading, getUrl } = useGet(
+    'https://kev-pi.herokuapp.com/api/mock/entities',
+    {
+      cors: 'no-cors',
+      headers: {
+        total: 200,
+        page: 1,
+        schema: JSON.stringify({
+          from: '{{name.findName}}',
+          timestamp: '{{date.recent}}',
+          subject: '{{lorem.sentence}}',
+          snippet: '{{lorem.lines}}',
+          fullMail: '{{lorem.paragraphs}}',
+          email: '{{internet.email}}',
+        }),
+      },
+    }
+  )
+  const [menu, setMenu] = useState(false)
+  if ((data === null || data.length === 0) && !loading) {
+    getUrl()
+  }
+  let messages = <div>Nope</div>
+  if (data && data.data) {
+    messages = data.data.map((msg, index) => {
+      return (
+        <MessageCard
+          active={msg == message}
+          key={`key-${index}`}
+          {...msg}
+          onClick={() => {
+            console.log('onclick')
+            setMessage(msg)
+          }}
+        />
+      )
+    })
+  }
+  console.info({data, error, loading,})
   return (
     <>
-      <nav className="navbar has-shadow">
-        <div className="container">
-          <div className="navbar-brand">
-            <a className="navbar-item" href="../">
-              <img
-                src="http://bulma.io/images/bulma-logo.png"
-                alt="Bulma: a modern CSS framework based on Flexbox"
-              />
-            </a>
+      <NavBar className="has-shadow">
+        <Container className="container">
+          <NavBarBrand
+            src="brightleafjs-logo.png"
+            alt="Brightleat Elements: React and Bulma powered UI framework"
+            onClick={e => {
+              setMenu(!menu)
+            }}
+          />
 
-            <div className="navbar-burger burger" data-target="navMenu">
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-
-          <div id="navMenu" className="navbar-menu">
-            <div className="navbar-end">
-              <div className="navbar-item has-dropdown is-active">
-                <a className="navbar-link">Account</a>
-
-                <div className="navbar-dropdown">
-                  <a className="navbar-item">Dashboard</a>
-                  <a className="navbar-item">Profile</a>
-                  <a className="navbar-item">Settings</a>
-                  <hr className="navbar-divider" />
-                  <div className="navbar-item">Logout</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-      <div className="columns" id="mail-app">
-        <aside className="column is-2 aside hero is-fullheight">
+          <NavBarMenu className={classnames({ 'is-active': menu })}>
+            <NavBarEnd className="navbar-end">
+              <NavBarDropDown title="Account">
+                <NavBarItem>
+                  <a>Dashboard</a>
+                </NavBarItem>
+                <NavBarItem>
+                  <a>Profile</a>
+                </NavBarItem>
+                <NavBarItem>
+                  <a>Settings</a>
+                </NavBarItem>
+                <NavBarDivider />
+                <NavBarItem>
+                  <div>Logout</div>
+                </NavBarItem>
+              </NavBarDropDown>
+            </NavBarEnd>
+          </NavBarMenu>
+        </Container>
+      </NavBar>
+      <Columns>
+        <Column
+          as="aside"
+          is="2"
+          isFullheight
+          className="aside hero is-fullheight"
+        >
           <div>
             <div className="compose has-text-centered">
-              <a className="button is-danger is-block is-bold">
+              <Button isDanger isAnchor className="is-block is-bold" href="#">
                 <span className="compose">Compose</span>
-              </a>
+              </Button>
             </div>
             <div className="main">
               <a href="#" className="item active">
-                <span className="icon">
-                  <i className="fa fa-inbox"></i>
-                </span>
+                <Icon fas icon="inbox" />
                 <span className="name">Inbox</span>
               </a>
               <a href="#" className="item">
-                <span className="icon">
-                  <i className="fa fa-star"></i>
-                </span>
+                <Icon fas icon="star" />
                 <span className="name">Starred</span>
               </a>
               <a href="#" className="item">
-                <span className="icon">
-                  <i className="fa fa-envelope-o"></i>
-                </span>
+                <Icon far icon="envelope" />
                 <span className="name">Sent Mail</span>
               </a>
               <a href="#" className="item">
-                <span className="icon">
-                  <i className="fa fa-folder-o"></i>
-                </span>
+                <Icon far icon="folder" />
                 <span className="name">Folders</span>
+              </a>
+              <a href="#HERO" className="item">
+                <Icon fab icon="superpowers" />
+                <span className="name">Hero</span>
+              </a>
+              <a href="#LANDING" className="item">
+                <Icon fas icon="plane-arrival" />
+                <span className="name">Landing</span>
+              </a>
+              <a href="#COVER" className="item">
+                <Icon fas icon="book-open" />
+                <span className="name">Cover</span>
+              </a>
+              <a href="#IMAGE_TIMELINE" className="item">
+                <Icon far icon="images" />
+                <span className="name">Insta</span>
+              </a>
+              <a href="#ADMIN" className="item">
+                <Icon fas icon="cogs" />
+                <span className="name">Admin</span>
+              </a>
+              <a href="#CHEATSHEET" className="item">
+                <Icon fas icon="code" />
+                <span className="name">CheatSheet</span>
+              </a>
+              <a href="#BLOG" className="item">
+                <Icon fas icon="blog" />
+                <span className="name">Blog</span>
               </a>
             </div>
           </div>
-        </aside>
-        <div
+        </Column>
+        <Column
           className="column is-4 messages hero is-fullheight"
           id="message-feed"
         >
-          <div className="action-buttons">
-            <div className="control is-grouped">
-              <a className="button is-small">
-                <i className="fa fa-chevron-down"></i>
-              </a>
-              <a className="button is-small">
-                <i className="fa fa-refresh"></i>
-              </a>
-            </div>
-            <div className="control is-grouped">
-              <a className="button is-small">
-                <i className="fa fa-inbox"></i>
-              </a>
-              <a className="button is-small">
-                <i className="fa fa-exclamation-circle"></i>
-              </a>
-              <a className="button is-small">
-                <i className="fa fa-trash-o"></i>
-              </a>
-            </div>
-            <div className="control is-grouped">
-              <a className="button is-small">
-                <i className="fa fa-folder"></i>
-              </a>
-              <a className="button is-small">
-                <i className="fa fa-tag"></i>
-              </a>
-            </div>
-            <div className="control is-grouped pg">
-              <div className="title">
-                paginate.pointer.start-paginate.pointer.end of paginate.total
-              </div>
-              <a className="button is-link">
-                <i className="fa fa-chevron-left"></i>
-              </a>
-              <a className="button is-link">
-                <i className="fa fa-chevron-right"></i>
-              </a>
-            </div>
-          </div>
+          <Control className="action-buttons">
+            <Control isGrouped className="control is-grouped">
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="chevron-down" />
+              </Button>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="sync" />
+              </Button>
+            </Control>
+            <Control isGrouped>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="inbox" />
+              </Button>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="exclamation-circle" />
+              </Button>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="trash-alt" />
+              </Button>
+            </Control>
+            <Control isGrouped>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="folder" />
+              </Button>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="tag" />
+              </Button>
+            </Control>
+            <Control isGrouped className="pg">
+              <Title as="div">1-10 of 100</Title>
+
+              <Button isLink isAnchor>
+                <BaseIcon fa icon="chevron-left" />
+              </Button>
+              <Button isLink isAnchor>
+                <BaseIcon fa icon="chevron-right" />
+              </Button>
+            </Control>
+          </Control>
 
           <div className="inbox-messages" id="inbox-messages">
-            <div
-              v-for="(msg, index) in messages"
-              className="card"
-              v-bind:id="'msg-card-'+index"
-              v-on:click="showMessage(msg,index)"
-              v-bind:data-preview-id="index"
-            >
-              <div className="card-content">
-                <div className="msg-header">
-                  <span className="msg-from">
-                    <small>From: msg.from </small>
-                  </span>
-                  <span className="msg-timestamp"></span>
-                  <span className="msg-attachment">
-                    <i className="fa fa-paperclip"></i>
-                  </span>
-                </div>
-                <div className="msg-subject">
-                  <span className="msg-subject">
-                    <strong id="fake-subject-1"> msg.subject</strong>
-                  </span>
-                </div>
-                <div className="msg-snippet">
-                  <p id="fake-snippet-1">msg.snippet</p>
-                </div>
-              </div>
-            </div>
+            {messages}
           </div>
-        </div>
-        <div
-          className="column is-6 message hero is-fullheight is-hidden"
+        </Column>
+        <Column
+          is="6"
+          isHidden={message.picked}
+          className="message hero is-fullheight"
           id="message-pane"
         >
           <div className="action-buttons">
-            <div className="control is-grouped">
-              <a className="button is-small">
-                <i className="fa fa-inbox"></i>
-              </a>
-              <a className="button is-small">
-                <i className="fa fa-exclamation-circle"></i>
-              </a>
-              <a className="button is-small">
-                <i className="fa fa-trash-o"></i>
-              </a>
-            </div>
-            <div className="control is-grouped">
-              <a className="button is-small">
-                <i className="fa fa-exclamation-circle"></i>
-              </a>
-              <a className="button is-small">
-                <i className="fa fa-trash-o"></i>
-              </a>
-            </div>
-            <div className="control is-grouped">
-              <a className="button is-small">
-                <i className="fa fa-folder"></i>
-              </a>
-              <a className="button is-small">
-                <i className="fa fa-tag"></i>
-              </a>
-            </div>
+            <Control isGrouped>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="inbox" />
+              </Button>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="exclamation-circle" />
+              </Button>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="trash-alt" />
+              </Button>
+            </Control>
+            <Control isGrouped>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="exclamation-circle" />
+              </Button>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="trash-alt" />
+              </Button>
+            </Control>
+            <Control isGrouped>
+              <Button isSmall isAnchor>
+                <BaseIcon fa icon="folder" />
+              </Button>
+              <Button isSmall>
+                <BaseIcon fa icon="tag" />
+              </Button>
+            </Control>
           </div>
-          <div className="box message-preview">
+          <Box className="message-preview">
             <div className="top">
               <div className="avatar">
                 <img src="https://placehold.it/128x128" />
               </div>
               <div className="address">
-                <div className="name">John Smith</div>
-                <div className="email">someone@gmail.com</div>
+                <div className="name">{message.from}</div>
+                <div className="email">{message.email}</div>
               </div>
               <hr />
-              <div className="content"></div>
+              <Content className="content">{message.fullMail}</Content>
             </div>
-          </div>
-        </div>
-      </div>
-      <footer className="footer">
-        <div className="container">
-          <div className="content has-text-centered">
+          </Box>
+        </Column>
+      </Columns>
+      <Footer>
+        <Container>
+          <Content hasTextCentered>
             <p>
-              <strong>Bulma Templates</strong> by{' '}
-              <a href="https://github.com/dansup">Daniel Supernault</a>. The
-              source code is licensed
-              <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+              <strong>Original Bulma Templates</strong> by{' '}
+              <a href="https://github.com/dansup">Daniel Supernault</a>.
+              <strong>Brightleaf/Elements</strong> by{' '}
+              <a href="https://github.com/kevnz">Kevin Isom</a>.
             </p>
             <p>
-              <a
-                className="icon"
-                href="https://github.com/dansup/bulma-templates"
-              >
-                <i className="fa fa-github"></i>
+              <a className="icon" href="https://github.com/brightleaf/elements">
+                <BaseIcon fab icon="github" />
               </a>
             </p>
-          </div>
-        </div>
-      </footer>
+          </Content>
+        </Container>
+      </Footer>
     </>
   )
 }
+
+export default InboxTemplate
