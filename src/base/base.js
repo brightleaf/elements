25856153b9b8fcb73f2c70bc3839fe913b49d800
Kevin Isom <kevin.isom@gmail.com>
2019-08-12 20:43:16 +1200

    export function as forwardRef

--- src/base/base.js
+++ src/base/base.js
@@ -1,32 +1,35 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import classnames from 'classnames'
 import PropTypes from 'prop-types'
 import { allTheClasses } from '../modifiers'
 import htmlAttributes from '../element-attributes'
 
-export const Base = ({ as: Element, className, children, ...props }) => {
-  const classes = allTheClasses(props)
-  const { onClick } = props
+export const Base = forwardRef(
+  ({ as: Element, className, children, ...props }, ref) => {
+    const classes = allTheClasses(props)
+    const { onClick } = props
 
-  const clean = htmlAttributes['*']
-    .concat(htmlAttributes[Element] || [])
-    .reduce((objs, current) => {
-      if (props[current]) {
-        objs[current] = props[current]
-      }
-      return objs
-    }, {})
+    const clean = htmlAttributes['*']
+      .concat(htmlAttributes[Element] || [])
+      .reduce((objs, current) => {
+        if (props[current]) {
+          objs[current] = props[current]
+        }
+        return objs
+      }, {})
 
-  return (
-    <Element
-      className={classnames(className, classes)}
-      onClick={onClick}
-      {...clean}
-    >
-      {children}
-    </Element>
-  )
-}
+    return (
+      <Element
+        ref={ref}
+        className={classnames(className, classes)}
+        onClick={onClick}
+        {...clean}
+      >
+        {children}
+      </Element>
+    )
+  }
+)
 
 Base.propTypes = {
   as: PropTypes.node,
