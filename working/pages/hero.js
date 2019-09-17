1ad7b864803ba4e46da12097af9f4bb5372b3648
Kevin Isom <kevin.isom@gmail.com>
2019-09-18 11:19:07 +1200

    show code for hero

--- working/pages/hero.js
+++ working/pages/hero.js
@@ -2,6 +2,8 @@
 /* eslint-disable spellcheck/spell-checker */
 import React from 'react'
 import {
+  Column,
+  Columns,
   Breadcrumb,
   BreadcrumbItem,
   Hero,
@@ -12,6 +14,9 @@ import {
   Title,
 } from '../../src'
 import { LayoutTabs } from '../components/tabs'
+import { Snippet } from '../components/snippet'
+import { CodeCollapse } from '../components/collapse'
+import Highlight from '../components/highlighter'
 export default () => {
   return (
     <Section>
@@ -41,44 +46,159 @@ export default () => {
       </div>
 
       <Container>
-        <Hero>
-          <Container>
-            <Title as="p">Hero title</Title>
-            <SubTitle as="p">Hero subtitle</SubTitle>
-          </Container>
-        </Hero>
+        <Snippet>
+          <Columns>
+            <Column isHalf>
+              <Hero>
+                <HeroBody>
+                  <Title as="p">Hero title</Title>
+                  <SubTitle as="p">Hero subtitle</SubTitle>
+                </HeroBody>
+              </Hero>
+            </Column>
+            <Column isHalf>
+              <Highlight>{`
+import React from 'react'
+import { Hero, HeroBody, Title, SubTitle } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Hero>
+      <HeroBody>
+        <Title as="p">Hero title</Title>
+        <SubTitle as="p">Hero subtitle</SubTitle>
+      </HeroBody>
+    </Hero>
+  )
+}
+              `}</Highlight>
+            </Column>
+          </Columns>
+        </Snippet>
       </Container>
       <Container>
-        <Hero isDanger>
-          <Container>
-            <Title is="1">Hero title</Title>
-            <SubTitle>Hero subtitle</SubTitle>
-          </Container>
-        </Hero>
+        <Snippet>
+          <Columns>
+            <Column isHalf>
+              <Hero isDanger>
+                <HeroBody>
+                  <Title is="1">Hero title</Title>
+                  <SubTitle>Hero subtitle</SubTitle>
+                </HeroBody>
+              </Hero>
+            </Column>
+            <Column isHalf>
+              <Highlight>{`
+import React from 'react'
+import { Hero, HeroBody, Title, SubTitle } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Hero isDanger>
+      <HeroBody>
+        <Title is="1">Hero title</Title>
+        <SubTitle>Hero subtitle</SubTitle>
+      </HeroBody>
+    </Hero>
+  )
+}
+              `}</Highlight>
+            </Column>
+          </Columns>
+        </Snippet>
       </Container>
       <Container>
-        <Hero isPrimary>
-          <Container>
-            <Title as="p">Hero title</Title>
-            <SubTitle as="p">Hero subtitle</SubTitle>
-          </Container>
-        </Hero>
+        <Snippet>
+          <Columns>
+            <Column isHalf>
+              <Hero isPrimary>
+                <HeroBody>
+                  <Title as="p">Hero title</Title>
+                  <SubTitle as="p">Hero subtitle</SubTitle>
+                </HeroBody>
+              </Hero>
+            </Column>
+            <Column isHalf>
+              <Highlight>{`
+import React from 'react'
+import { Hero, HeroBody, Title, SubTitle } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Hero isPrimary>
+      <HeroBody>
+        <Title as="p">Hero title</Title>
+        <SubTitle as="p">Hero subtitle</SubTitle>
+      </HeroBody>
+    </Hero>
+  )
+}
+              `}</Highlight>
+            </Column>
+          </Columns>
+        </Snippet>
       </Container>
       <Container>
-        <Hero isPrimary isBold>
-          <Container>
-            <Title>Hero title</Title>
-            <SubTitle>Hero subtitle</SubTitle>
-          </Container>
-        </Hero>
+        <Snippet>
+          <Columns>
+            <Column isHalf>
+              <Hero isPrimary isBold>
+                <HeroBody>
+                  <Title>Hero title</Title>
+                  <SubTitle>Hero subtitle</SubTitle>
+                </HeroBody>
+              </Hero>
+            </Column>
+            <Column isHalf>
+              <Highlight>{`
+import React from 'react'
+import { Hero, HeroBody, Title, SubTitle } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Hero isPrimary isBold>
+      <HeroBody>
+        <Title>Hero title</Title>
+        <SubTitle>Hero subtitle</SubTitle>
+      </HeroBody>
+    </Hero>
+  )
+}
+              `}</Highlight>
+            </Column>
+          </Columns>
+        </Snippet>
       </Container>
       <Container>
-        <Hero isInfo isBold isFullHeight>
-          <Container>
-            <Title>Hero title</Title>
-            <SubTitle>Hero subtitle</SubTitle>
-          </Container>
-        </Hero>
+        <Snippet>
+          <Columns>
+            <Column isHalf>
+              <Hero isInfo isBold isFullHeight>
+                <Container>
+                  <Title>Hero title</Title>
+                  <SubTitle>Hero subtitle</SubTitle>
+                </Container>
+              </Hero>
+            </Column>
+            <Column isHalf>
+              <Highlight>{`
+import React from 'react'
+import { Hero, HeroBody, Title, SubTitle } from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <Hero isInfo isBold isFullHeight>
+      <HeroBody>
+        <Title>Hero title</Title>
+        <SubTitle>Hero subtitle</SubTitle>
+      </HeroBody>
+    </Hero>
+  )
+}
+              `}</Highlight>
+            </Column>
+          </Columns>
+        </Snippet>
       </Container>
     </Section>
   )
