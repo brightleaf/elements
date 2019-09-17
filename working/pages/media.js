25045c6ca3ffcc96310dc8051b43ca82daaecfbf
Kevin Isom <kevin.isom@gmail.com>
2019-09-18 11:18:53 +1200

    fix up the media object demo

--- working/pages/media.js
+++ working/pages/media.js
@@ -4,14 +4,18 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   Delete,
+  Image,
+  Level,
+  LevelItem,
+  LevelLeft,
   MediaObject,
   MediaLeft,
   MediaContent,
   MediaRight,
+  Content,
   Container,
   Column,
   Columns,
-  FullColumn,
   Icon,
   Hero,
   HeroBody,
@@ -20,7 +24,9 @@ import {
   SubTitle,
 } from '../../src'
 import { LayoutTabs } from '../components/tabs'
-
+import { Snippet } from '../components/snippet'
+import { CodeCollapse } from '../components/collapse'
+import Highlight from '../components/highlighter'
 export default () => {
   return (
     <Section>
@@ -51,158 +57,357 @@ export default () => {
       </div>
 
       <Container>
-        <Columns>
-          <Column isFourFifths>
-            <MediaObject>
-              <MediaLeft title="Component" hasIcon>
-                {' '}
-                <p className="image is-64x64">
-                  <img src="https://bulma.io/images/placeholders/128x128.png" />
-                </p>
-              </MediaLeft>
-              <MediaContent>
-                <div className="content">
-                  <p>
-                    <strong>John Smith</strong> <small>@johnsmith</small>{' '}
-                    <small>31m</small>
-                    <br />
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Proin ornare magna eros, eu pellentesque tortor vestibulum
-                    ut. Maecenas non massa sem. Etiam finibus odio quis feugiat
-                    facilisis.
-                  </p>
-                </div>
-                <nav className="level is-mobile">
-                  <div className="level-left">
-                    <a className="level-item">
-                      <Icon isSmall fas icon="reply" />
-                    </a>
-                    <a className="level-item">
-                      <Icon isSmall fas icon="retweet" />
-                    </a>
-                    <a className="level-item">
-                      <Icon isSmall fas icon="heart" />
-                    </a>
-                  </div>
-                </nav>
-              </MediaContent>
-              <MediaRight>
-                <Delete></Delete>
-              </MediaRight>
-            </MediaObject>
-          </Column>
-        </Columns>
+        <Snippet>
+          <Columns>
+            <Column isHalf>
+              <MediaObject>
+                <MediaLeft title="Component" hasIcon>
+                  <Image
+                    is64
+                    src="https://bulma.io/images/placeholders/128x128.png"
+                  />
+                </MediaLeft>
+                <MediaContent>
+                  <Content className="content">
+                    <p>
+                      <strong>John Smith</strong> <small>@johnsmith</small>{' '}
+                      <small>31m</small>
+                      <br />
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Proin ornare magna eros, eu pellentesque tortor vestibulum
+                      ut. Maecenas non massa sem. Etiam finibus odio quis
+                      feugiat facilisis.
+                    </p>
+                  </Content>
+                  <Level isMobile>
+                    <LevelLeft>
+                      <LevelItem>
+                        <a>
+                          <Icon isSmall fas icon="reply" />
+                        </a>
+                      </LevelItem>
+                      <LevelItem>
+                        <a>
+                          <Icon isSmall fas icon="retweet" />
+                        </a>
+                      </LevelItem>
+                      <LevelItem>
+                        <a>
+                          <Icon isSmall fas icon="heart" />
+                        </a>
+                      </LevelItem>
+                    </LevelLeft>
+                  </Level>
+                </MediaContent>
+                <MediaRight>
+                  <Delete></Delete>
+                </MediaRight>
+              </MediaObject>
+            </Column>
+            <Column isHalf>
+              <CodeCollapse>
+                <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import {
+  MediaObject, MediaLeft, Content
+} from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <MediaObject>
+      <MediaLeft title="Component" hasIcon>
+        <Image
+          is64
+          src="https://bulma.io/images/placeholders/128x128.png"
+        />
+      </MediaLeft>
+      <MediaContent>
+        <Content className="content">
+          <p>
+            <strong>John Smith</strong> <small>@johnsmith</small>{' '}
+            <small>31m</small>
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Proin ornare magna eros, eu pellentesque tortor vestibulum
+            ut. Maecenas non massa sem. Etiam finibus odio quis
+            feugiat facilisis.
+          </p>
+        </Content>
+        <Level isMobile>
+          <LevelLeft>
+            <LevelItem>
+              <a>
+                <Icon isSmall fas icon="reply" />
+              </a>
+            </LevelItem>
+            <LevelItem>
+              <a>
+                <Icon isSmall fas icon="retweet" />
+              </a>
+            </LevelItem>
+            <LevelItem>
+              <a>
+                <Icon isSmall fas icon="heart" />
+              </a>
+            </LevelItem>
+          </LevelLeft>
+        </Level>
+      </MediaContent>
+      <MediaRight>
+        <Delete></Delete>
+      </MediaRight>
+    </MediaObject>
+  )
+}
+              `}</Highlight>
+              </CodeCollapse>
+            </Column>
+          </Columns>
+        </Snippet>
         <br />
         <hr />
         <br />
-        <FullColumn>
-          <MediaObject>
-            <MediaLeft>
-              <p className="image is-64x64">
-                <img src="https://bulma.io/images/placeholders/128x128.png" />
-              </p>
-            </MediaLeft>
-            <MediaContent>
-              <div className="content">
-                <p>
-                  <strong>Barbara Middleton</strong>
-                  <br />
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  porta eros lacus, nec ultricies elit blandit non. Suspendisse
-                  pellentesque mauris sit amet dolor blandit rutrum. Nunc in
-                  tempus turpis.
-                  <br />
-                  <small>
-                    <a>Like</a> · <a>Reply</a> · 3 hrs
-                  </small>
-                </p>
-              </div>
-
+        <Snippet>
+          <Columns>
+            <Column isHalf>
               <MediaObject>
                 <MediaLeft>
-                  <p className="image is-48x48">
-                    <img src="https://bulma.io/images/placeholders/96x96.png" />
+                  <p className="image is-64x64">
+                    <img src="https://bulma.io/images/placeholders/128x128.png" />
                   </p>
                 </MediaLeft>
                 <MediaContent>
-                  <div className="content">
+                  <Content>
                     <p>
-                      <strong>Sean Brown</strong>
+                      <strong>Barbara Middleton</strong>
                       <br />
-                      Donec sollicitudin urna eget eros malesuada sagittis.
-                      Pellentesque habitant morbi tristique senectus et netus et
-                      malesuada fames ac turpis egestas. Aliquam blandit nisl a
-                      nulla sagittis, a lobortis leo feugiat.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Duis porta eros lacus, nec ultricies elit blandit non.
+                      Suspendisse pellentesque mauris sit amet dolor blandit
+                      rutrum. Nunc in tempus turpis.
                       <br />
                       <small>
-                        <a>Like</a> · <a>Reply</a> · 2 hrs
+                        <a>Like</a> · <a>Reply</a> · 3 hrs
                       </small>
                     </p>
-                  </div>
+                  </Content>
 
                   <MediaObject>
-                    Vivamus quis semper metus, non tincidunt dolor. Vivamus in
-                    mi eu lorem cursus ullamcorper sit amet nec massa.
+                    <MediaLeft>
+                      <Image
+                        is48
+                        rsc="https://bulma.io/images/placeholders/96x96.png"
+                      />
+                    </MediaLeft>
+                    <MediaContent>
+                      <Content>
+                        <p>
+                          <strong>Sean Brown</strong>
+                          <br />
+                          Donec sollicitudin urna eget eros malesuada sagittis.
+                          Pellentesque habitant morbi tristique senectus et
+                          netus et malesuada fames ac turpis egestas. Aliquam
+                          blandit nisl a nulla sagittis, a lobortis leo feugiat.
+                          <br />
+                          <small>
+                            <a>Like</a> · <a>Reply</a> · 2 hrs
+                          </small>
+                        </p>
+                      </Content>
+
+                      <MediaObject>
+                        Vivamus quis semper metus, non tincidunt dolor. Vivamus
+                        in mi eu lorem cursus ullamcorper sit amet nec massa.
+                      </MediaObject>
+
+                      <MediaObject>
+                        Morbi vitae diam et purus tincidunt porttitor vel vitae
+                        augue. Praesent malesuada metus sed pharetra euismod.
+                        Cras tellus odio, tincidunt iaculis diam non, porta
+                        aliquet tortor.
+                      </MediaObject>
+                    </MediaContent>
                   </MediaObject>
 
                   <MediaObject>
-                    Morbi vitae diam et purus tincidunt porttitor vel vitae
-                    augue. Praesent malesuada metus sed pharetra euismod. Cras
-                    tellus odio, tincidunt iaculis diam non, porta aliquet
-                    tortor.
+                    <MediaLeft>
+                      <Image
+                        is48
+                        src="https://bulma.io/images/placeholders/96x96.png"
+                      />
+                    </MediaLeft>
+                    <MediaContent>
+                      <Content>
+                        <p>
+                          <strong>Kayli Eunice </strong>
+                          <br />
+                          Sed convallis scelerisque mauris, non pulvinar nunc
+                          mattis vel. Maecenas varius felis sit amet magna
+                          vestibulum euismod malesuada cursus libero. Vestibulum
+                          ante ipsum primis in faucibus orci luctus et ultrices
+                          posuere cubilia Curae; Phasellus lacinia non nisl id
+                          feugiat.
+                          <br />
+                          <small>
+                            <a>Like</a> · <a>Reply</a> · 2 hrs
+                          </small>
+                        </p>
+                      </Content>
+                    </MediaContent>
                   </MediaObject>
                 </MediaContent>
               </MediaObject>
-
               <MediaObject>
                 <MediaLeft>
-                  <p className="image is-48x48">
-                    <img src="https://bulma.io/images/placeholders/96x96.png" />
-                  </p>
+                  <Image
+                    is64
+                    src="https://bulma.io/images/placeholders/128x128.png"
+                  />
                 </MediaLeft>
                 <MediaContent>
-                  <div className="content">
-                    <p>
-                      <strong>Kayli Eunice </strong>
-                      <br />
-                      Sed convallis scelerisque mauris, non pulvinar nunc mattis
-                      vel. Maecenas varius felis sit amet magna vestibulum
-                      euismod malesuada cursus libero. Vestibulum ante ipsum
-                      primis in faucibus orci luctus et ultrices posuere cubilia
-                      Curae; Phasellus lacinia non nisl id feugiat.
-                      <br />
-                      <small>
-                        <a>Like</a> · <a>Reply</a> · 2 hrs
-                      </small>
+                  <div className="field">
+                    <p className="control">
+                      <textarea
+                        className="textarea"
+                        placeholder="Add a comment..."
+                      ></textarea>
+                    </p>
+                  </div>
+                  <div className="field">
+                    <p className="control">
+                      <button className="button">Post comment</button>
                     </p>
                   </div>
                 </MediaContent>
               </MediaObject>
-            </MediaContent>
-          </MediaObject>
-          <MediaObject>
-            <MediaLeft>
-              <p className="image is-64x64">
-                <img src="https://bulma.io/images/placeholders/128x128.png" />
+            </Column>
+            <Column isHalf>
+              <CodeCollapse>
+                <Highlight className="javascript" languages={['javascript']}>{`
+import React from 'react'
+import {
+  MediaObject, MediaLeft, Content
+} from '@brightleaf/elements'
+
+export default () => {
+  return (
+    <MediaObject>
+      <MediaLeft>
+        <p className="image is-64x64">
+          <img src="https://bulma.io/images/placeholders/128x128.png" />
+        </p>
+      </MediaLeft>
+      <MediaContent>
+        <Content>
+          <p>
+            <strong>Barbara Middleton</strong>
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Duis porta eros lacus, nec ultricies elit blandit non.
+            Suspendisse pellentesque mauris sit amet dolor blandit
+            rutrum. Nunc in tempus turpis.
+            <br />
+            <small>
+              <a>Like</a> · <a>Reply</a> · 3 hrs
+            </small>
+          </p>
+        </Content>
+
+        <MediaObject>
+          <MediaLeft>
+            <Image
+              is48
+              rsc="https://bulma.io/images/placeholders/96x96.png"
+            />
+          </MediaLeft>
+          <MediaContent>
+            <Content>
+              <p>
+                <strong>Sean Brown</strong>
+                <br />
+                Donec sollicitudin urna eget eros malesuada sagittis.
+                Pellentesque habitant morbi tristique senectus et
+                netus et malesuada fames ac turpis egestas. Aliquam
+                blandit nisl a nulla sagittis, a lobortis leo feugiat.
+                <br />
+                <small>
+                  <a>Like</a> · <a>Reply</a> · 2 hrs
+                </small>
               </p>
-            </MediaLeft>
-            <MediaContent>
-              <div className="field">
-                <p className="control">
-                  <textarea
-                    className="textarea"
-                    placeholder="Add a comment..."
-                  ></textarea>
-                </p>
-              </div>
-              <div className="field">
-                <p className="control">
-                  <button className="button">Post comment</button>
-                </p>
-              </div>
-            </MediaContent>
-          </MediaObject>
-        </FullColumn>
+            </Content>
+
+            <MediaObject>
+              Vivamus quis semper metus, non tincidunt dolor. Vivamus
+              in mi eu lorem cursus ullamcorper sit amet nec massa.
+            </MediaObject>
+
+            <MediaObject>
+              Morbi vitae diam et purus tincidunt porttitor vel vitae
+              augue. Praesent malesuada metus sed pharetra euismod.
+              Cras tellus odio, tincidunt iaculis diam non, porta
+              aliquet tortor.
+            </MediaObject>
+          </MediaContent>
+        </MediaObject>
+
+        <MediaObject>
+          <MediaLeft>
+            <Image
+              is48
+              src="https://bulma.io/images/placeholders/96x96.png"
+            />
+          </MediaLeft>
+          <MediaContent>
+            <Content>
+              <p>
+                <strong>Kayli Eunice </strong>
+                <br />
+                Sed convallis scelerisque mauris, non pulvinar nunc
+                mattis vel. Maecenas varius felis sit amet magna
+                vestibulum euismod malesuada cursus libero. Vestibulum
+                ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia Curae; Phasellus lacinia non nisl id
+                feugiat.
+                <br />
+                <small>
+                  <a>Like</a> · <a>Reply</a> · 2 hrs
+                </small>
+              </p>
+            </Content>
+          </MediaContent>
+        </MediaObject>
+      </MediaContent>
+    </MediaObject>
+    <MediaObject>
+      <MediaLeft>
+        <Image
+          is64
+          src="https://bulma.io/images/placeholders/128x128.png"
+        />
+      </MediaLeft>
+      <MediaContent>
+        <div className="field">
+          <p className="control">
+            <textarea
+              className="textarea"
+              placeholder="Add a comment..."
+            ></textarea>
+          </p>
+        </div>
+        <div className="field">
+          <p className="control">
+            <button className="button">Post comment</button>
+          </p>
+        </div>
+      </MediaContent>
+    </MediaObject>
+  )
+}
+            `}</Highlight>
+              </CodeCollapse>
+            </Column>
+          </Columns>
+        </Snippet>
       </Container>
     </Section>
   )
