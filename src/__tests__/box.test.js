3fc69d87de8be903d13dad270ebccb8d9e9cd66a
Kevin Isom <kevin.isom@gmail.com>
2019-11-21 10:17:15 +1300

    Use base and add more tests

--- src/__tests__/box.test.js
+++ src/__tests__/box.test.js
@@ -26,4 +26,50 @@ describe('Box component', () => {
     expect(container.firstChild).toHaveClass('box')
     expect(container.firstChild).toHaveClass('boxed-in')
   })
+  it('should render with has-background-primary when hasBackgroundPrimary set', () => {
+    const { container } = render(
+      <Box className="boxed-in" hasBackgroundPrimary>
+        <div>Trapped in a Box</div>
+      </Box>
+    )
+
+    expect(container.firstChild).toHaveClass('box')
+    expect(container.firstChild).toHaveClass('has-background-primary')
+  })
+  test.each([
+    ['has-background-primary', 'hasBackgroundPrimary'],
+    ['has-background-success', 'hasBackgroundSuccess'],
+    ['has-background-warning', 'hasBackgroundWarning'],
+    ['has-background-danger', 'hasBackgroundDanger'],
+    ['has-background-info', 'hasBackgroundInfo'],
+    ['has-background-white', 'hasBackgroundWhite'],
+    ['has-background-black', 'hasBackgroundBlack'],
+    ['has-background-light', 'hasBackgroundLight'],
+    ['has-background-dark', 'hasBackgroundDark'],
+    ['has-background-link', 'hasBackgroundLink'],
+    ['has-background-black-bis', 'hasBackgroundBlackBis'],
+    ['has-background-black-ter', 'hasBackgroundBlackTer'],
+    ['has-background-grey-darker', 'hasBackgroundGreyDarker'],
+    ['has-background-grey-dark', 'hasBackgroundGreyDark'],
+    ['has-background-grey', 'hasBackgroundGrey'],
+    ['has-background-grey-light', 'hasBackgroundGreyLight'],
+    ['has-background-grey-lighter', 'hasBackgroundGreyLighter'],
+    ['has-background-white-ter', 'hasBackgroundWhiteTer'],
+    ['has-background-white-bis', 'hasBackgroundWhiteBis'],
+  ])(
+    'The background color class %i is added when %i is set)',
+    (className, propertyName) => {
+      const prop = {}
+      prop[propertyName] = true
+      const { container } = render(
+        <Box className="boxed-in" {...prop}>
+          <div>Trapped in a Box</div>
+        </Box>
+      )
+
+      expect(container.firstChild).toHaveClass('box')
+      expect(container.firstChild).toHaveClass('boxed-in')
+      expect(container.firstChild).toHaveClass(className)
+    }
+  )
 })
--- src/box/box.js
+++ src/box/box.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import classnames from 'classnames'
-const Box = ({ children, className }) => {
-  return <div className={classnames('box', className)}>{children}</div>
+import { Base } from '../base'
+
+const Box = ({ children, className, ...props }) => {
+  return (
+    <Base className={classnames('box', className)} {...props}>
+      {children}
+    </Base>
+  )
 }
 
 Box.propTypes = {}
+
+Box.defaultProps = {
+  as: 'div',
+}
 
 export { Box }
 
